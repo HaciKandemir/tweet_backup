b4e6abf760084f90bbe47e33f541cde3f823a87a
HaciKandemir <hacikandemir2000@hotmail.com>
2020-12-06 15:49:36 +0300

    - yazılım iyileştirmesi yapıldı gereksiz kodlar düzenlendi - kod okunaklığı arttırılmaya çalışıldı. - tweetleri saklama formu değişti - log çıktı örneği eklendi - tweets çıktı örneği düzenlendi

--- Main.py
+++ Main.py
@@ -1,153 +1,81 @@
 import tweepy
-import sys
 import json
-import datetime
+import os
+from datetime import datetime
 import time
 import logging
-# dotenv start
+import sys
 from dotenv import load_dotenv
-import os
+
+
+class CustomDataFormat(dict):
+    def __init__(self, data):
+        self["tweet_id"] = data["id"]
+        self["screen_name"] = data["user"]["screen_name"]
+        self["name"] = data["user"]["name"]
+        self["user_id"] = data["user"]["id"]
+        self["full_text"] = data["full_text"]
+        self["created_at"] = str(datetime.strptime(data["created_at"], '%a %b %d %H:%M:%S +0000 %Y'))
+        self["eklenme_tarihi"] = str(datetime.utcnow().replace(microsecond=0))
+
+
+logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s-%(levelname)s: %(message)s ',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+logging.info('############# Program Başladı #############')
+
 load_dotenv()
-# dotenv end
+CONSUMER_KEY = os.getenv('consumer_key')
+CONSUMER_SECRET = os.getenv('consumer_secret')
+screen_name = os.getenv('screenName')
 
-# module_name, package_name, ClassName, method_name, ExceptionName,
-# function_name, GLOBAL_CONSTANT_NAME, global_var_name, instance_var_name,
-# function_parameter_name, local_var_name
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, parser=tweepy.parsers.JSONParser())
 
-logging.basicConfig(filename='log.txt', level=logging.INFO)
-
-consumer_key = os.getenv('consumer_key')
-consumer_secret = os.getenv('consumer_secret')
-access_token = os.getenv('access_token')
-access_token_secret = os.getenv('access_token_secret')
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# Tweepy API, istek sınırına ulaştığında otomatik bekleme (uyku) yapar ve sona ermesinden sonra devam eder
-api = tweepy.API(auth, wait_on_rate_limit=True,
-                 wait_on_rate_limit_notify=True)  # tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 if not api:
-    print("Bağlantıda sorun var")
+    logging.critical('twitter bağlantısında sorun var')
     sys.exit(-1)
 
-# toplam ne kadar tivit gelecek
-maxTweets = 10
-# apiden tek sorguda kaç tivit gelecek
-tweetsPerQry = 5
-allTweetCount = 0
-fName = 'tweets.txt'
-screenName = os.getenv('screenName')
+tweets_per_qry = 5
+total_new_tweet_count = 0
+new_tweet_count = 0
+db_filename = "tweets.txt"
+tekrar = 0
 
+customized_api_response = list()
+local_tweets = list()
 
-def datetime_to_srt(o):
-    if isinstance(o, datetime.datetime):
-        return str(o.replace(microsecond=0))
+# dosyanın içinde veri varsa onu yeni veriler ile karşılaştırmak için listeye ekliyor
+if os.stat(db_filename).st_size != 0:
+    with open(db_filename, 'r', encoding="utf-8") as db_file:
+        local_tweets = json.load(db_file)
 
-
-# twitterden tweetleri alıp geri döndürüyor
-def get_timeline_tweets(screen_name, tweet_per_qry_count, tweet_mode='extended', since_id=None, max_id=None):
-    return api.user_timeline(screen_name=screen_name, count=tweet_per_qry_count, tweet_mode=tweet_mode,
-                             since_id=since_id or None, max_id=str(max_id-1) if max_id > 0 else None)
-
-
-class TweetFormat(object):
-    def __init__(self, data):
-        self.__dict__ = data
-
-    # 'if in' için
-    def __contains__(self, key):
-        return key in self.__dict__
-
-    def __setitem__(self, key, value):
-        self.__dict__[key] = value
-
-    # 'for in' için
-    def __iter__(self):
-        return iter(self.__dict__.items())
-
-    def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__, indent=4, ensure_ascii=False)
-
-
-# dosyadaki json formatındaki verileri okuyup döndürüyor
-def read_json_file(file_name=fName):
-    try:
-        with open(file_name, 'r', encoding='utf-8') as f:
-            convert = json.load(f)
-            return TweetFormat(convert)
-    except ValueError:
-        return {}
-
-
-def convert_tweet_format(ob):
-    deger = json.loads(json.dumps(ob))
-    return TweetFormat(deger)
-
-
-def write_json_file(json_data, file_name=fName):
-    with open(file_name, 'w', encoding="utf-8") as f:
-        f.write(
-            json_data
-        )
-
-
-# gelen geriyi dosyada kaydedilecek şekilde kişiselleştirip json olarak geri döndürüyor
-def tweet_json_format(json_tweet):
-    return {"screen_name": json_tweet.user.screen_name, "name": json_tweet.user.name,
-            "user_id": json_tweet.user.id, "tweet_id": json_tweet.id, "full_text": json_tweet.full_text,
-            "created_at": datetime_to_srt(json_tweet.created_at),
-            "eklenme_tarihi": datetime_to_srt(datetime.datetime.utcnow())}
-
-
-tweetDb = {}
-apiReqTweets = {}
-newTweets = {}
-loadFile = True
-dongu = 0
-# Belirli bir ID'den sonraki sonuçlar gerekliyse, since_id'i bu ID'ye ayarlayın.
-# varsayılan olarak alt sınır yoktur, API'nin izin verdiği kadar geriye gidin
-sinceId = None
-# Sonuçlar yalnızca belirli bir kimliğin altındaysa, max_id'i bu kimliğe ayarlayın.
-# varsayılan olarak üst sınır yoksa, arama sorgusuyla eşleşen en son tweet ile başlayın.
-maxId = 0
-logging.info("###############################Program başladı####################################")
 while True:
-    print("{0}. döngü".format(dongu))
-    if loadFile:
-        loadFile = False
-        tweetDb = read_json_file()
-    tweetCount = 0
+    # api_res kendisi list içinde 0,1,2 indexlerinde dict şeklinde tweetleri saklıyor
+    api_res = api.user_timeline(screen_name=screen_name, count=tweets_per_qry, tweet_mode="extended")
 
-    print("Downloading max {0} new tweets".format(maxTweets))
-    while tweetCount < maxTweets:
-        try:
-            apiReqTweets = get_timeline_tweets(
-                screen_name=screenName, tweet_per_qry_count=tweetsPerQry, since_id=sinceId, max_id=maxId
-            )
-            if not apiReqTweets:
-                print("No more tweets found")
-                break
-            for apiTweet in apiReqTweets:
-                if not apiTweet.id_str in tweetDb:
-                    newTweets[apiTweet.id] = tweet_json_format(apiTweet)
-            tweetCount += len(apiReqTweets)
-            allTweetCount += len(newTweets)
-            print("Download {0} tweet, Found {1} new tweets".format(tweetCount, len(newTweets)))
-            #maxId = apiReqTweets[-1].id
-            sinceId = apiReqTweets[0].id
-        except tweepy.TweepError as e:
-            logging.error(str(e))
-            # Just exit if any error
-            print("some error : " + str(e))
-            break
-    for tweet in newTweets:
-        tweetDb.__setitem__(tweet, newTweets[tweet])
+    # api_res içerisindeki ihtiyacım olan verilerden daha minik bir dict oluşturup listeye ekliyorum
+    for api_res_json in api_res:
+        customized_api_response.append(CustomDataFormat(api_res_json))
 
-    if not len(newTweets) < 1:
-        write_json_file(tweetDb.to_json())
-        loadFile = True
-        print(
-            "Found {0} new tweets and all found {1} tweets, Saved to {2}".format(len(newTweets), allTweetCount, fName))
-    logging.info("{0}-) <----{1}----> yeni tweet eklendi. Toplam = {2} ({3})".format(dongu, len(newTweets), allTweetCount, datetime.datetime.utcnow().replace(microsecond=0)))
-    newTweets.clear()
-    dongu = dongu+1
+    # daha önceden dosyaya yazdığım veriler ile api den gelen tweetleri karşılaştırıyorum.
+    # yeni tweetler varsa onları da dosyadaki verilerden oluşan listeye ekliyorum
+    for api_tweet in customized_api_response:
+        if not api_tweet["tweet_id"] in [ids["tweet_id"] for ids in local_tweets]:
+            local_tweets.append(dict(api_tweet))
+            new_tweet_count += 1
+
+    # bu döngüde yeni tweet var ise en son yazılan tweet en sona gelecek şekilde sıralıyorum.
+    # daha sonra bunu listeyi dosyaya yazdırıyorum.
+    if new_tweet_count > 0:
+        # listenin içindeki dict leri yazdırıyor
+        with open(db_filename, 'w', encoding="utf-8") as db_file:
+            local_tweets = sorted(local_tweets, key=lambda x: x["tweet_id"])
+            json.dump(local_tweets, db_file, indent=4, ensure_ascii=False)
+
+    logging.info('%s-> bu döngüde eklenen: %s, toplam eklenen: %s', tekrar, new_tweet_count, total_new_tweet_count)
+
+    total_new_tweet_count += new_tweet_count
+    tekrar += 1
+    new_tweet_count = 0
+    customized_api_response.clear()
     time.sleep(20)
