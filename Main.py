09f28f2803e8b6c7790115a954c4b3ee29c68f09
HaciKandemir <hacikandemir2000@hotmail.com>
2020-12-25 18:50:57 +0300

    flask eklendi. TweetLoop kodu uygulama içinden çalıştırılmaya çalışıldı

--- Main.py
+++ Main.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, json
+from threading import Thread
+from try_thread import loop
 app = Flask(__name__)
+
+first_get = True
 
 @app.route('/')
 def main():
+    global first_get
+    if first_get:
+        t1 = Thread(target=loop)
+        t1.start()
+        first_get = False
+        
     with open("tweets.txt", "r") as f:
         tweets = f.read()
     return render_template('index.html', tweets=json.loads(tweets))
@@ -12,3 +22,9 @@ def log():
     with open("log.txt", "r") as f:
         logs = f.readlines()
     return render_template('log.html', logs=logs)
+
+# @app.route('/dongu')
+# def dongu():
+#    Res1 = Thread(target=loop)
+#    Res1.start()
+#   return 'loop başladı'
